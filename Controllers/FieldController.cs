d1a8b5ccf6bcca9dd28f51eff986ff003739187e
hoangnguyen-w <hoangnguyen2car@gmail.com>
2022-09-25 17:44:38 +0700

    xong 4 function: Create Field, Update Field, delete Filed, Search by name Field, Search by ID field

---
+++ Controllers/FieldController.cs
@@ -0,0 +1,76 @@
+﻿using BookStoreManage.DTO;
+using BookStoreManage.Entity;
+using BookStoreManage.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStoreManage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FieldController : ControllerBase
+    {
+        private readonly IFieldRepository _repository;
+        private readonly BookManageContext _context;
+
+        public FieldController(BookManageContext context, IFieldRepository repository)
+        {
+            _context = context;
+            _repository = repository;
+        }
+
+        // GET: api/<FieldController>
+        [HttpGet]
+        public IActionResult GetAlls()
+        {
+            return Ok(_context.Fields.ToList());
+        }
+
+        // GET api/<FieldController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var result = _repository.getByID(id);
+            if(id == null)
+            {
+                return NotFound();
+            }
+            return Ok(result.Result);
+        }
+
+        // POST api/<FieldController>
+        [HttpPost]
+        public IActionResult Post(string name, string description)
+        {
+            _repository.CreateField(name, description);
+            //return Ok(_repository.ShowLastOfList);
+            return Ok(_context.Fields.ToList());
+        }
+
+        // PUT api/<FieldController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, FieldDTO fields)
+        {
+            _repository.EditField(id, fields);
+            if(id == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        // DELETE api/<FieldController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.DeleteField(id);
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_repository);
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreManage.Entity;
+
+namespace BookStoreManage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly BookManageContext _context;
+
+        public TestController(BookManageContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Test
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Field>>> GetFields()
+        {
+            return await _context.Fields.ToListAsync();
+        }
+
+        // GET: api/Test/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Field>> GetField(int id)
+        {
+            var @field = await _context.Fields.FindAsync(id);
+
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            return @field;
+        }
+
+        // PUT: api/Test/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutField(int id, Field @field)
+        {
+            if (id != @field.FieldID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(@field).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FieldExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Test
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Field>> PostField(Field @field)
+        {
+            _context.Fields.Add(@field);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetField", new { id = @field.FieldID }, @field);
+        }
+
+        // DELETE: api/Test/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteField(int id)
+        {
+            var @field = await _context.Fields.FindAsync(id);
+            if (@field == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fields.Remove(@field);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FieldExists(int id)
+        {
+            return _context.Fields.Any(e => e.FieldID == id);
+        }
+    }
+}
