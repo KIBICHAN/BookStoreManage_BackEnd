d568b76855b882a6287e92a47486ddcfb0ec995c
KIBICHAN <kibichan52@gmail.com>
2022-10-07 13:12:50 +0700

    Thieu ABC

--- IRepository/IBookRepository.cs
+++ IRepository/IBookRepository.cs
@@ -6,15 +6,11 @@ namespace BookStoreManage.IRepository
     public interface IBookRepository
     {
         Task CreateBook(BookDTO bookDTO);
-
         Task EditBook(int bookID, BookDTO books);
-
         Task DeleteBook(int bookID);
-
         Task<List<Book>> getAllBook();
-
         Task<Book> getByID(int bookID);
-
         Task<List<Book>> getByName(string bookName);
+        Task<List<BookDTO>> ImportExcel(IFormFile file);
     }
 }
