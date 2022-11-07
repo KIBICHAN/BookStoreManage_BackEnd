40a761c039ca37570f7ea7792b98d28b5f5abe1e
KIBICHAN <kibichan52@gmail.com>
2022-11-07 13:04:55 +0700

    Add author to best seller!

--- Migrations/20221101144918_sql.Designer.cs
+++ Migrations/20221101144918_sql.Designer.cs
@@ -4,6 +4,7 @@ using BookStoreManage.Entity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStoreManage.Migrations
 {
     [DbContext(typeof(BookManageContext))]
-    partial class BookManageContextModelSnapshot : ModelSnapshot
+    [Migration("20221101144918_sql")]
+    partial class sql
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
